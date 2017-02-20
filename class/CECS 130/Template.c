022b56abe01799daaf2b20a3457a0b81b108a453
Lukas Armstrong <Lucas.J.Armstrong@gmail.com>
2017-02-20 16:09:40 -0500

    The start of tracking things with GIT

---
+++ class/CECS 130/Template.c
@@ -0,0 +1,10 @@
+/*********************************/
+/*     G. Lukas J. Armstrong     */
+/*          CECS 130-02          */
+/*         Assignment 02         */
+/*********************************/
+#include <stdio.h>
+int main(){
+  
+  return 0;
+}
