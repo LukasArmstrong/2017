82f7236710cf4b989b8d79603c18939393effeb0
Lukas Armstrong <Lucas.J.Armstrong@gmail.com>
2017-03-06 18:00:38 -0500

    Add underline to links and worked on headers

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ var names=["The Ultimate Reese\'s Cupcake",
            "Peanut Butter Hi-Hat Cupcakes",
            "Chocolate Cookies and Cream Cupcakes",
            "Caramel Brownies",
-           "Peanut Butter Chocolate chip Brownies",
+           "Peanut Butter Chocolate Chip Brownies",
            "Sinckers Brownies",
            "Cookies and Cream Cookies",
            "Double Chocolate Cookies",
@@ -21,18 +21,12 @@ var gen3= `" alt="">
     <div id="myModal" class="modal">
       <!-- Modal content -->
       <div class="modal-content">
-        <div class="modal-header">
-          <span class="close">&times;</span>
-          <h2>Modal Header</h2>
-        </div>
+        <span class="close">&times;</span>
         <div class="modal-body">
           <img src="img/`;
 var gen4= `" alt="" id="img01">
           <p>Some text in the Modal Body</p>
           <p>Some other text...</p>
-        </div>
-        <div class="modal-footer">
-          <h3>Modal Footer</h3>
         </div>
       </div>
     </div>
